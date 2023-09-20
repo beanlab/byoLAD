0651b80f11b732cfd9b98ec163af3a6884353712
Spencer M <spencer.g.moffat@gmail.com>
2023-09-20 13:41:50 -0600

    Add custom prompt command option for file review. Make menu options context aware.
    This makes it so that the whole file review options only appear when no text is selected.
    It also makes it so that the selected code review options only appear when there is a selection.

--- src/commands/getReviewFileCodeCustomPromptCommand.ts
+++ src/commands/getReviewFileCodeCustomPromptCommand.ts
@@ -1,16 +1,39 @@
 import * as vscode from "vscode";
+import {
+  CODE_REVIEW_PROGRESS_TITLE,
+  INVALID_USER_INPUT_ERROR_MESSAGE,
+} from "./constants";
+import { doCompletion, displayDiff, getUserPrompt } from "../helpers";
 import { SettingsProvider } from "../helpers/SettingsProvider";
 
+/**
+ * Collects a user inputted prompt to query the model for a reviewed, edited version of the current file contents.
+ * Displays a diff of the active editor document and the completion response.
+ */
 export const getReviewFileCodeCustomPromptCommand = (
   settingsProvider: SettingsProvider,
 ): vscode.Disposable => {
-  return vscode.commands.registerCommand(
+  const reviewFileCodeCommand = vscode.commands.registerCommand(
     "vs-code-ai-extension.reviewFileCodeCustomPrompt",
     async () => {
-      // TODO: Implement this command
-      console.log(settingsProvider.getReviewFileCodePrompt());
-      console.log("reviewFileCodeCustomPromptCommand");
-      return;
+      const code = vscode.window.activeTextEditor?.document.getText();
+      const modelInstruction = await getUserPrompt();
+      const progressTitle = CODE_REVIEW_PROGRESS_TITLE;
+
+      if (!modelInstruction) {
+        vscode.window.showErrorMessage(INVALID_USER_INPUT_ERROR_MESSAGE);
+        return;
+      }
+
+      doCompletion(
+        settingsProvider.getCompletionModel(),
+        code,
+        modelInstruction,
+        progressTitle,
+        (completion, activeEditor) =>
+          displayDiff(completion.completion, activeEditor),
+      );
     },
   );
+  return reviewFileCodeCommand;
 };

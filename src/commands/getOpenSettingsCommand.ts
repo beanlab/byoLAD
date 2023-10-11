ed78373c04b25d133c5bf8b7932af5191eb6140f
MacKenzie Fisher <89889979+m13467@users.noreply.github.com>
2023-10-11 17:01:18 -0600

    Added Walkthrough

---
+++ src/commands/getOpenSettingsCommand.ts
@@ -0,0 +1,15 @@
+import * as vscode from "vscode";
+
+/**
+ * Queries the model for a reviewed, edited version of the current file contents.
+ * Presents the suggestions to the user according to their settings.
+ */
+export const getOpenSettingsCommand = (): vscode.Disposable => {
+  const openSettingsCommand = vscode.commands.registerCommand(
+    "vscode-byolad.openSettingsCommand",
+    async () => {
+      vscode.commands.executeCommand('workbench.action.openSettings','byoLAD');
+    },
+  );
+  return openSettingsCommand;
+};

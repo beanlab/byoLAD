e88d150320de7c951a9380c8ab8eec5d5f966e29
Spencer M <spencer.g.moffat@gmail.com>
2023-11-13 11:58:00 -0700

    Make code blocks use syntax highlighting

--- webview-ui/src/components/Message.tsx
+++ webview-ui/src/components/Message.tsx
@@ -1,25 +1,72 @@
+import { VSCodeDivider } from "@vscode/webview-ui-toolkit/react";
+import Markdown from "react-markdown";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { darcula } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import byo_LAD_icon from "../circle_byo_LAD.png";
 import { UserIcon } from "./UserIcon";
-import { ChatRole, MessageBlock } from "../utilities/ChatModel";
-import { VSCodeDivider } from "@vscode/webview-ui-toolkit/react";
+import { ChatRole, CodeBlock, MessageBlock } from "../utilities/ChatModel";
 
 interface MessageProps {
   role: ChatRole;
   messageBlocks: MessageBlock[];
 }
-
 export const Message: React.FC<MessageProps> = ({ role, messageBlocks }) => {
   return (
     <div>
       <VSCodeDivider role="separator" />
       <div className="message">
         <MessageHeader role={role} />
-        {messageBlocks.map((messageBlock) => (
-          <Block messageBlock={messageBlock} />
-        ))}
+        <div className="message-block">
+          {messageBlocks.map((messageBlock) => {
+            if (messageBlock.type === "text") {
+              return (
+                <TextMessageBlock>{messageBlock.content}</TextMessageBlock>
+              );
+            } else if (messageBlock.type === "code") {
+              return (
+                <CodeMessageBlock
+                  languageId={(messageBlock as CodeBlock).languageId}
+                >
+                  {messageBlock.content}
+                </CodeMessageBlock>
+              );
+            } else {
+              throw new Error("Unknown message block type");
+            }
+          })}
+        </div>
       </div>
     </div>
   );
+};
+
+interface TextMessageBlockProps {
+  children: string;
+}
+const TextMessageBlock: React.FC<TextMessageBlockProps> = ({ children }) => {
+  return <Markdown>{children}</Markdown>;
+};
+
+interface CodeMessageBlockProps {
+  languageId: string | undefined;
+  children: string;
+}
+const CodeMessageBlock: React.FC<CodeMessageBlockProps> = ({
+  languageId,
+  children,
+}) => {
+  if (!languageId) {
+    let markdown: string = "```";
+    markdown += "\n" + children + "\n```";
+    return <Markdown>{markdown}</Markdown>;
+  } else {
+    const markdown = children;
+    return (
+      <SyntaxHighlighter language={languageId} style={darcula}>
+        {markdown}
+      </SyntaxHighlighter>
+    );
+  }
 };
 
 interface MessageHeaderProps {
@@ -30,7 +77,6 @@ const MessageHeader: React.FC<MessageHeaderProps> = ({ role }) => {
     case ChatRole.User:
       return (
         <div className="header">
-          {/* <img src={user_icon} alt={"user"} className="user-icon" /> */}
           <UserIcon />
           <div className="role-name">You</div>
         </div>
@@ -48,10 +94,3 @@ const MessageHeader: React.FC<MessageHeaderProps> = ({ role }) => {
       return null;
   }
 };
-
-interface BlockProps {
-  messageBlock: MessageBlock;
-}
-const Block: React.FC<BlockProps> = ({ messageBlock }) => (
-  <div className="message-block">{messageBlock.content}</div>
-);

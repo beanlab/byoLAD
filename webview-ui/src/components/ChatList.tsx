1eff462af273e24b277ed23c199f6d0ebd91d814
Rebekah Daniels <math919191@gmail.com>
2024-01-22 14:02:11 -0700

    can edit chat name front end

--- webview-ui/src/components/ChatList.tsx
+++ webview-ui/src/components/ChatList.tsx
@@ -2,6 +2,8 @@ import { Conversation } from "../utilities/ChatModel";
 import { ExtensionMessenger } from "../utilities/ExtensionMessenger";
 import { VSCodeButton } from "@vscode/webview-ui-toolkit/react";
 import { VSCodeBadge } from "@vscode/webview-ui-toolkit/react";
+import React, {  useState } from 'react';
+
 
 interface ChatListProps {
   chatList: Conversation[];
@@ -26,29 +28,91 @@ export const ChatList = ({ chatList, changeActiveChat }: ChatListProps) => {
       name = "Empty Chat";
     } else if (conversation.messages[0].content[0].type !== "code") {
       name = conversation.messages[0].content[0].content;
+      //TODO update this so that it actually comes up with something meaningful
     } else {
       name = "undefined";
       const temp = conversation.messages[1].content[0].content;
       const temp2 = temp.split(".");
       name = temp2[0];
     }
+
+    const [editing, setEditing] = useState(false);
+    const [value, setValue] = useState(name);
+
+
+    const handleClick = () => {
+      console.log("handling click!")
+      setEditing(!editing);
+    };
+    
+    // const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    //   console.log("setting")
+    //   setValue(event.target.value);
+    // };
+
+    const handleInputChange = (event: React.FormEvent<HTMLElement>) => {
+      
+      setValue((event.target as HTMLInputElement).value);
+      console.log(value)
+
+    };
+
     return (
       <div className="convo">
-        <div
+        
+
+        {editing ? (
+          <input value={value} onChange={handleInputChange} />
+
+        ) : (
+          <div
           onClick={() => handleOnClick(conversation)}
           key={conversation.id}
           className="convo-id"
         >
-          {name}
+          {/* {name} */}
+          {value}
+      </div>
+
+        )}
+        <div>
+
+        {editing ? (
+          <VSCodeButton
+            appearance="icon"
+            aria-label="Done editing conversation title"
+            title="Done editing conversation title"
+            onClick={() => handleClick()}
+            >
+            <i className="codicon codicon-check"></i>
+          </VSCodeButton>
+
+        ) : (
+          <VSCodeButton
+            appearance="icon"
+            aria-label="Edit conversation title"
+            title="Edit conversation title"
+            onClick={() => handleClick()}
+            >
+            <i className="codicon codicon-edit"></i>
+          </VSCodeButton>
+
+        )}
+          
+
+          <VSCodeButton
+            appearance="icon"
+            aria-label="Delete conversation"
+            title="Delete conversation"
+            onClick={() => extensionMessenger.deleteConversation(conversation.id)}
+          >
+            <i className="codicon codicon-trash"></i>
+          </VSCodeButton>
+
+
+
+
         </div>
-        <VSCodeButton
-          appearance="icon"
-          aria-label="Delete conversation"
-          title="Delete conversation"
-          onClick={() => extensionMessenger.deleteConversation(conversation.id)}
-        >
-          <i className="codicon codicon-trash"></i>
-        </VSCodeButton>
       </div>
     );
   });
